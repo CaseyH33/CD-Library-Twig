a7e2381c97e61893fabe7ed0ba447ba2497eb7de
Julio Diaz & Casey Heitz <student@epicodus.com>
2015-08-06 11:37:05 -0700

    initial commit with basic setup

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/cd.php";
+
+    session_start();
+
+    if(empty($_SESSION['list_of_cds'])) {
+        $_SESSION['list_of_cds'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+
+?>
